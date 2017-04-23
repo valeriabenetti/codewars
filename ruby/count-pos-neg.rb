5e9b1eb5ad218006d480edc4e37fbe741754d7d3
Valeria Benetti <vbenetti46@gmail.com>
2017-04-23 09:20:54 -0400

    Count of Positive numbers and negative numbers

---
+++ ruby/count-pos-neg.rb
@@ -0,0 +1,12 @@
+def count_positives_sum_negatives(numbers)
+  count = 0
+  sum = 0
+  numbers.each do |number|
+    if number < 0
+      sum += number.to_i
+    elsif number > 0
+      count += 1
+    end
+  end
+  return array = [count, sum]
+end
--- ruby/opposites.rb
+++ ruby/opposites.rb
@@ -1,3 +1,5 @@
+# https://newline.theironyard.com/paths/1034/units/6275/assignments/15417
+
 def lovefunc( flower1, flower2 )
   if flower1.even? && flower2.odd?
     return true
